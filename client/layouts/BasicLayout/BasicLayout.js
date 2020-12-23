a5bc14fd873dd9817b5086c0ac440c7f8395309e
devHectorGa <contacto@devHector.Ga>
2020-12-22 20:30:20 -0500

    First commit

---
+++ client/layouts/BasicLayout/BasicLayout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+
+export default function BasicLayout({ children }) {
+  return (
+    <Container fluid className="basic-layout">
+      <Container className="content">{children}</Container>
+    </Container>
+  );
+}
