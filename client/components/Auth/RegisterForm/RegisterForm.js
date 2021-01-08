2c8c6f1ea7ebf0a75a621f25406f18bccf798b41
devHectorGa <contacto@devHector.Ga>
2021-01-07 19:33:27 -0500

    Formulario de registro de usuarios

--- client/components/Auth/RegisterForm/RegisterForm.js
+++ client/components/Auth/RegisterForm/RegisterForm.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
+import { Form, Button } from 'semantic-ui-react';
+
 export default function RegisterForm(props) {
   const { showLoginForm } = props;
   return (
-    <div>
-      <h1>Estamos en el registro de usuarios</h1>
-      <button onClick={showLoginForm}>Ir al login</button>
-    </div>
+    <Form className="login-form">
+      <Form.Input name="name" type="text" placeholder="Nombre" />
+      <Form.Input name="lastname" type="text" placeholder="Apellidos" />
+      <Form.Input name="usename" type="text" placeholder="Nombre de usuario" />
+      <Form.Input name="email" type="text" placeholder="Correo electronico" />
+      <Form.Input name="password" type="password" placeholder="Contraseña" />
+      <div className="actions">
+        <Button type="button" basic>
+          Iniciar Sesión
+        </Button>
+        <Button type="submit" className="submit">
+          Registrar
+        </Button>
+      </div>
+    </Form>
   );
 }
