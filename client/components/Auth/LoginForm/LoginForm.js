3af0a15309d8462bfbda7c9b7c967985470942d3
devHectorGa <contacto@devHector.Ga>
2021-03-10 11:17:30 -0500

    Recuperar contraseña

--- client/components/Auth/LoginForm/LoginForm.js
+++ client/components/Auth/LoginForm/LoginForm.js
@@ -3,7 +3,7 @@ import { Form, Button } from 'semantic-ui-react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { toast } from 'react-toastify';
-import { loginApi } from '../../../api/user';
+import { loginApi, resetPasswordApi } from '../../../api/user';
 import useAuth from '../../../hooks/useAuth';
 
 export default function LoginForm(props) {
@@ -25,6 +25,18 @@ export default function LoginForm(props) {
       setLoading(false);
     },
   });
+
+  const resetPassword = () => {
+    formik.setErrors({});
+
+    const validateEmail = Yup.string().email().required();
+
+    if (!validateEmail.isValidSync(formik.values.identifier)) {
+      formik.setErrors({ identifier: true });
+    } else {
+      resetPasswordApi(formik.values.identifier);
+    }
+  };
 
   return (
     <Form className="login-form" onSubmit={formik.handleSubmit}>
@@ -50,7 +62,9 @@ export default function LoginForm(props) {
           <Button className="submit" type="submit" loading={loading}>
             Entrar
           </Button>
-          <Button type="button">¿Has olvidado la contraseña?</Button>
+          <Button type="button" onClick={resetPassword}>
+            ¿Has olvidado la contraseña?
+          </Button>
         </div>
       </div>
     </Form>
