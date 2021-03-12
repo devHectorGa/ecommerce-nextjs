868f52dac3214a8d0192562f1efe3a3e68649b8a
devHectorGa <contacto@devHector.Ga>
2021-03-11 22:06:08 -0500

    Boton para cerrar sesión

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,13 +3,15 @@ import '../scss/global.scss';
 import { ToastContainer } from 'react-toastify';
 import AuthContext from '../context/AuthContext';
 import jwtDecode from 'jwt-decode';
-import { setToken, getToken } from '../api/token';
+import { useRouter } from 'next/router';
+import { setToken, getToken, removeToken } from '../api/token';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function MyApp({ Component, pageProps }) {
   const [auth, setAuth] = useState(undefined);
   const [reloadUser, setReloadUser] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     const token = getToken();
@@ -32,11 +34,19 @@ export default function MyApp({ Component, pageProps }) {
     });
   };
 
+  const logout = () => {
+    if (auth) {
+      removeToken();
+      setAuth(null);
+      router.push('/');
+    }
+  };
+
   const authData = useMemo(
     () => ({
       auth,
       login,
-      logout: () => null,
+      logout,
       setReloadUser,
     }),
     [auth]
