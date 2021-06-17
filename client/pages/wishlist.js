32e7cee1d839c6ce5d4207d21e90f8536f458101
devHectorGa <contacto@devHector.Ga>
2021-06-17 13:45:32 -0500

    Renderizando la lista de juegos

--- client/pages/wishlist.js
+++ client/pages/wishlist.js
@@ -3,15 +3,26 @@ import { size, forEach } from 'lodash';
 import BasicLayout from '../layouts/BasicLayout';
 import { getFavoriteApi } from '../api/favorite';
 import useAuth from '../hooks/useAuth';
+import { Loader } from 'semantic-ui-react';
+import ListGames from '../components/ListGames';
 
 export default function wishlist() {
   const [games, setGames] = useState(null);
+  const [loading, setLoading] = useState(false);
   const { auth, logout } = useAuth();
 
   useEffect(() => {
     (async () => {
+      setLoading(true);
       const response = await getFavoriteApi(auth.idUser, logout);
-      setGames(response);
+      if (response.length) {
+        const gamesList = [];
+        response.forEach((data) => gamesList.push(data.game));
+        setGames(gamesList);
+      } else {
+        setGames([]);
+      }
+      setLoading(false);
     })();
   }, []);
 
@@ -20,7 +31,14 @@ export default function wishlist() {
       <div className="wishlist__block">
         <div className="title">Lista de deseos</div>
         <div className="data">
-          <p>Lista de juegos</p>
+          {loading ? (
+            <Loader active={loading}>Cargando juegos</Loader>
+          ) : (
+            <>
+              {games?.length && <ListGames className="content" games={games} />}
+              {!games?.length && <h3>No tienes ningún juego en tu lista</h3>}
+            </>
+          )}
         </div>
       </div>
     </BasicLayout>
