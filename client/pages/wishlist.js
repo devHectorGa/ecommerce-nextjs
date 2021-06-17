f30a5dc87f2dc513ed9361581f54daa77a4f7c5a
devHectorGa <contacto@devHector.Ga>
2021-06-17 13:26:03 -0500

    Creando la pagina de favoritos

---
+++ client/pages/wishlist.js
@@ -0,0 +1,14 @@
+import BasicLayout from '../layouts/BasicLayout';
+
+export default function wishlist() {
+  return (
+    <BasicLayout className="wishlist">
+      <div className="wishlist__block">
+        <div className="title">Lista de deseos</div>
+        <div className="data">
+          <p>Lista de juegos</p>
+        </div>
+      </div>
+    </BasicLayout>
+  );
+}
