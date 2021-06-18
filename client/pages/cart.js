d55fc6caa1dd0da9b1cb3fc505ef29031379fae9
devHectorGa <contacto@devHector.Ga>
2021-06-18 09:26:57 -0500

    Creando la pagina del carrito

---
+++ client/pages/cart.js
@@ -0,0 +1,8 @@
+import BasicLayout from '../layouts/BasicLayout';
+export default function Cart() {
+  return (
+    <BasicLayout className="cart">
+      <h1>Carrito</h1>
+    </BasicLayout>
+  );
+}
