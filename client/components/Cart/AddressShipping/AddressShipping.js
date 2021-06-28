61940db1776d1eb1899a11372d14d56d8675c13a
devHectorGa <hector.garzon@10pearls.com>
2021-06-28 14:47:47 -0500

    Mostrando la lista de direcciones de envío

--- client/components/Cart/AddressShipping/AddressShipping.js
+++ client/components/Cart/AddressShipping/AddressShipping.js
@@ -15,7 +15,43 @@ export default function AddressShipping() {
       const response = await getAddressesApi(auth.idUser, logout);
       setAddresses(response || []);
     })();
-  }, [input]);
+  }, []);
 
-  return <div></div>;
+  return (
+    <div className="address-shipping">
+      <div className="title">Dirección de envío</div>
+      <div className="data">
+        {addresses?.length ? (
+          <Grid>
+            {map(addresses, (address) => (
+              <Grid.Column key={address.id} mobile={16} tablet={8} computer={4}>
+                <Address address={address} />
+              </Grid.Column>
+            ))}
+          </Grid>
+        ) : (
+          <h3>
+            No hay ninguna dirección creada{' '}
+            <Link href="/account">
+              <a>añadir tu primera dirección</a>
+            </Link>
+          </h3>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function Address({ address }) {
+  return (
+    <div className="address">
+      <p>{address.title}</p>
+      <p>{address.name}</p>
+      <p>{address.address}</p>
+      <p>
+        {address.city}, {address.state}, {address.postalCode}
+      </p>
+      <p>{address.phone}</p>
+    </div>
+  );
 }
