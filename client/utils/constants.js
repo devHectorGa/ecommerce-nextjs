1431f1133c683d1b6c4e43eba3f2e1a1f7b18470
devHectorGa <contacto@devHector.Ga>
2021-11-16 22:58:58 -0500

    Desplegando Next en Netlify

--- client/utils/constants.js
+++ client/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_PATH = 'http://localhost:1337';
+export const BASE_PATH = 'https://ecommerce-next-devhectorga.herokuapp.com';
 export const TOKEN = 'token';
 export const CART = 'CART';
 export const STRIPE_TOKEN = 'pk_test_j47TcWe1wf4Wrjqdi6GJMpeN008wR0cGMl';
